416aafaf9d552ff40c7337dc1b67f7d8b08a3d17
yphacker <yphacker@163.com>
2018-09-04 00:53:40 +0800

    添加设计模式的单例模式

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # designPatterns
 设计模式
+
+使用IntelliJ IDEA编辑的项目
+采用Springboot+Maven构建的项目
+用来学习设计模式
+后序如有相关的java应用，也会更新到此项目中
---
+++ src/main/java/com/yphacker/designPatterns/singleton/subclass/HotterSingleton.java
@@ -0,0 +1,11 @@
+package com.yphacker.designPatterns.singleton.subclass;
+
+public class HotterSingleton extends Singleton {
+	// useful instance variables here
+ 
+	private HotterSingleton() {
+		super();
+	}
+ 
+	// useful methods here
+}
